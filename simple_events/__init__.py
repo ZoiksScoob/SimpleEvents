4e4a092e709616a3b26ebe664f8e45ba05e38beb
ZoiksScoob <alex_jc_brown@hotmail.com>
2020-09-05 11:58:02 +0100

    Alter app instantiation to api instantiation

--- simple_events/__init__.py
+++ simple_events/__init__.py
@@ -3,9 +3,10 @@ from flask import Flask
 from flask_uuid import FlaskUUID
 
 
-def create_app(test_config=None):
+def create_app(app_name, test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(app_name, instance_relative_config=True)
+    
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'simple_events.sqlite'),
