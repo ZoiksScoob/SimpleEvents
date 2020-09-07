7cdd8ed91557d4f79bb642db94b1b9c1d7d32a5c
ZoiksScoob <alex_jc_brown@hotmail.com>
2020-09-07 16:59:17 +0100

    Add lines breaks to Logout resource for readability

--- simple_events/apis/auth.py
+++ simple_events/apis/auth.py
@@ -171,18 +171,22 @@ class Logout(Resource):
 
         try:
             resp = User.decode_auth_token(auth_token)
+            
             if not isinstance(resp, str):
                 # mark the token as blacklisted
                 blacklist_token = BlacklistToken(token=auth_token)
+
                 try:
                     # insert the token
                     db.session.add(blacklist_token)
                     db.session.commit()
+
                     response_object = {
                         'status': 'success',
                         'message': 'Successfully logged out.'
                     }
                     return response_object, 200
+
                 except Exception as e:
                     response_object = {
                         'status': 'fail',
