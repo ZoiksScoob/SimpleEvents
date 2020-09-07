8375713420bf0addfbd534d5e55e483ab45748bb
ZoiksScoob <alex_jc_brown@hotmail.com>
2020-09-07 22:50:23 +0100

    Fix (remove) unneeded inner try except in Logout resource's post method

--- simple_events/apis/auth.py
+++ simple_events/apis/auth.py
@@ -175,23 +175,16 @@ class Logout(Resource):
                 # mark the token as blacklisted
                 blacklist_token = BlacklistToken(token=auth_token)
 
-                try:
-                    # insert the token
-                    db.session.add(blacklist_token)
-                    db.session.commit()
+                # insert the token
+                db.session.add(blacklist_token)
+                db.session.commit()
 
-                    response_object = {
-                        'status': 'success',
-                        'message': 'Successfully logged out.'
-                    }
-                    return response_object, 200
+                response_object = {
+                    'status': 'success',
+                    'message': 'Successfully logged out.'
+                }
+                return response_object, 200
 
-                except Exception as e:
-                    response_object = {
-                        'status': 'fail',
-                        'message': e
-                    }
-                    return response_object, 200
             else:
                 response_object = {
                     'status': 'fail',
