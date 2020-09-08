b9a6477fa98af59b3e2452258b56adec709efb52
ZoiksScoob <alex_jc_brown@hotmail.com>
2020-09-08 13:57:01 +0100

    Use models/__init__.py is intermediary file to ensure all models are imported beforedb imported into app and migration ext init so that migrate sees schema changes

--- simple_events/models/__init__.py
+++ simple_events/models/__init__.py
@@ -1,1 +1,6 @@
+from simple_events.models.db import db, bcrypt
+from simple_events.models.auth import User, BlacklistToken
+from simple_events.models.event import Event, Ticket
 
+# Imports into here so that imports of all the models are made
+# before db is imported into app.py and migration initialised.
