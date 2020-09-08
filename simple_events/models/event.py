28284d12eada91514afeef81ce97873b7c1f9a7e
ZoiksScoob <alex_jc_brown@hotmail.com>
2020-09-08 14:46:03 +0100

    Add __init__ method on db.Model's Ticket and Event to handle defaults, and remove declared defaults on columns

--- simple_events/models/event.py
+++ simple_events/models/event.py
@@ -11,11 +11,18 @@ class Event(db.Model):
     name = db.Column(db.String(255), unique=True, nullable=False)
     initial_number_of_tickets = db.Column(db.Integer, nullable=False)
     additional_number_of_tickets = db.Column(db.Integer, nullable=True)
-    guid = db.Column(db.BLOB, nullable=False, default=uuid.uuid4)
+    guid = db.Column(db.BLOB, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    date_created_utc = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) 
+    date_created_utc = db.Column(db.DateTime, nullable=False)
 
     db.UniqueConstraint(guid, name='uix__event__guid')
+
+    def __init__(self, name, initial_number_of_tickets, author_id):
+        self.name = name
+        self.initial_number_of_tickets = initial_number_of_tickets
+        self.author_id = author_id
+        self.guid = uuid.uuid4().bytes
+        self.date_created_utc = datetime.utcnow()
 
 
 class Ticket(db.Model):
@@ -23,9 +30,16 @@ class Ticket(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
-    guid = db.Column(db.BLOB, nullable=False, default=uuid.uuid4)
-    is_redeemed = db.Column(db.Boolean, default=False)
-    date_created_utc = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    guid = db.Column(db.BLOB, nullable=False)
+    is_redeemed = db.Column(db.Boolean)
+    date_created_utc = db.Column(db.DateTime, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     db.UniqueConstraint('guid', name='uix__ticket__guid')
+
+    def __init__(self, event_id, author_id):
+        self.event_id = event_id
+        self.guid = uuid.uuid4().bytes
+        self.is_redeemed = False
+        self.date_created_utc = datetime.utcnow()
+        self.author_id = author_id
