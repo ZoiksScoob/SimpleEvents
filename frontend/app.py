468fe8649649bc067864b5ecd526fe793d56ccdc
ZoiksScoob <alex_jc_brown@hotmail.com>
2020-09-09 09:24:44 +0100

    Add some stylesheets to frontend/app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,8 @@
 import dash
 
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 server = app.server
