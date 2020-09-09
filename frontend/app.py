22c626a80d821707caa3b82a986d10aec02247ee
ZoiksScoob <alex_jc_brown@hotmail.com>
2020-09-09 12:29:11 +0100

    Add check of auth token on url change, and force re-login if expired in frontend/index.py

--- frontend/app.py
+++ frontend/app.py
@@ -6,6 +6,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+app.config.suppress_callback_exceptions = True
+
 server = app.server
 
 api_url = os.environ.get('API_URL', 'http://localhost:5000/')
--- frontend/index.py
+++ frontend/index.py
@@ -15,23 +15,51 @@ app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='hidden-div-for-auth-page-redirect-callback', style={'display': 'none'}),
     dcc.Store(id='session', storage_type='session'),
-    html.Div(id='page-content')
+    html.Div([
+        html.Div([
+            'Your session has expired, please click to continue to login page.',
+            html.Button('Go to Login', id='session-expired-dialog-button')
+            ], 
+            id='session-expired-dialog',
+            style={'display': 'none'}),
+        html.Div(id='page-content')
+    ])
 ])
 
 
+def is_token_valid(session):
+    # Put session token validation after going to auth page
+    if session and session['token']:
+        headers = {'Authorization': session['token'], 'content-type': 'application/json'}
+        response = r.get(api_url + 'auth/status', headers=headers)
+        return response.status_code == 200
+    return False
+
+
 @app.callback(
-    Output('page-content', 'children'),
-    [Input('url', 'pathname')]
+    [
+        Output('page-content', 'children'),
+        Output('session-expired-dialog', 'style'),
+    ],
+    [Input('url', 'pathname')],
+    state=[State('session', 'data')]
 )
-def display_page(pathname):
-    if pathname in ('/auth', '', '/') :
-        return auth.layout
+def display_page(pathname, session):
+    hide_session_expiry_div = {'display': 'none'}
+
+    if pathname in ('/auth', '', '/'):
+        return auth.layout, hide_session_expiry_div
+
+    token_is_valid = is_token_valid(session)
+
+    if not token_is_valid:
+        return '', None
     elif pathname == '/event':
-        return event.layout
+        return event.layout, hide_session_expiry_div
     elif pathname == '/ticket':
-        return ticket.layout
+        return ticket.layout, hide_session_expiry_div
     else:
-        return '404: Page Not Found'
+        return '404: Page Not Found', hide_session_expiry_div
 
 
 @app.callback(
