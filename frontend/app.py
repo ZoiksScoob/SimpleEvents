834fc3ba7154f538f1cd28773fd57c7251ee0f95
ZoiksScoob <alex_jc_brown@hotmail.com>
2020-09-09 09:46:06 +0100

    Add API_URL environment variable, defaulting to server local development url

--- frontend/app.py
+++ frontend/app.py
@@ -1,3 +1,4 @@
+import os
 import dash
 
 
@@ -6,3 +7,5 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
+
+api_url = os.environ.get('API_URL', 'http://localhost:5000/')
